374b9f83a1a99a6333ed6d1e85764a0259db77e0
Nikita Parkhomenko <nikita.parkhom@gmail.com>
2020-09-29 16:28:44 +0300

    Tweet Item component was created

---
+++ src/components/TweetItem.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="tweet" @click="favouriteTweet(tweet.id)">
+
+    <div class="tweet__wrapper">
+      <h5 class="tweet__user">
+        @{{ userName }}
+      </h5>
+
+      <p class="tweet__content">
+        {{ tweet.content }}
+      </p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TweetItem',
+    props: {
+      userName: {
+        type: String,
+        required: true
+      },
+      tweet: {
+        type: Object,
+        required: true
+      }
+    },
+    methods: {
+      favouriteTweet(id) {
+        this.$emit('favourite', id)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .tweet {
+    width: 60%;
+    padding: 20px;
+    background-color: white;
+    border-radius: 8px;
+    border: 1px solid #dfe3e8;
+    box-sizing: border-box;
+    cursor: pointer;
+    transition: 0.4s all;
+    margin-bottom: 20px;
+  }
+
+  .tweet:hover {
+    transform: scale(1.05);
+    background-color: #d9eeff;
+  }
+
+  .tweet__user {
+    font-weight: bold;
+  }
+</style>
+
--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -3,7 +3,7 @@
 
     <div class="user-profile__user-panel">
 
-      <h1 class="user-profile__username">@{{ user.userName }}</h1>
+      <h2 class="user-profile__username">@{{ user.userName }}</h2>
       <p v-if="user.isAdmin" class="user-profile__admin-badge">Admin</p>
 
       <div class="user-profile__follower-count">
@@ -12,12 +12,25 @@
 
     </div>
 
+    <div class="user-profile__tweets-wrapper">
+      <TweetItem 
+        v-for="tweet in user.tweets" 
+        :key="tweet.id" 
+        :userName="user.userName" 
+        :tweet="tweet" 
+        @favourite="addToFavourites" 
+      />
+    </div>
+
   </div>
 </template>
 
 <script>
+import TweetItem from './TweetItem';
+
 export default {
   name: 'UserProfile',
+  components: { TweetItem },
   data() {
     return {
       followers: 100,
@@ -28,6 +41,10 @@ export default {
         email: 'nikita.parkhom@gmail.com',
         id: 1,
         isAdmin: false,
+        tweets: [
+          { id: 1, content: 'Thanks Tesla Team for great work on deliveries! For new owners, we super appreciate accommodating us on delivery timing! Definitely one of our toughest quarters in global logistics.' },
+          { id: 2, content: 'Completed a full duration test fire of the Raptor Vacuum engine at SpaceX’s rocket development facility in McGregor, Texas'},
+        ]
       }
     }
   },
@@ -39,6 +56,9 @@ export default {
   methods: {
     addFolowers() {
       this.followers++
+    },
+    addToFavourites(id) {
+      console.log(`Tweet #${id} added to favourites List`);
     }
   },
   mounted() {
@@ -49,15 +69,16 @@ export default {
 
 <style scoped>
 .user-profile {
-  display: grid;
-  grid-template-columns: 1fr 3fr;
-  width: 100%;
+  display: flex;
+  justify-content: start;
+  width: vw;
   padding: 50px 5%;
 }
 
 .user-profile__user-panel {
   display: flex;
   flex-direction: column;
+  width: 20%;
   padding: 20px;
   margin-right: 50px;
   background-color: white;
