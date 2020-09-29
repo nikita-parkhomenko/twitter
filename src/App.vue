68bf019cc1503889ddf7f29ecf4f1a284a2a6bce
Nikita Parkhomenko <nikita.parkhom@gmail.com>
2020-09-29 12:03:50 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    @{{ user.userName }} - {{ fullName }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      followers: 100,
+      user: {
+        userName: 'nekit_park',
+        firstName: 'Nikita',
+        lastName: 'Parkhomenko',
+        email: 'nikita.parkhom@gmail.com',
+        id: 1,
+        isAdmin: true,
+      }
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
